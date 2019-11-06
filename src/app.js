245e898e4a82d58a8d79894fc09779a7795b5594
Pedro Henrique <pedrohenrique@Pedros-iMac.local>
2019-11-05 21:46:02 -0300

    settings

--- src/app.js
+++ src/app.js
@@ -1,32 +1,31 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
-import * as Sentry from '@sentry/node'
-import Youch from 'youch'
-import 'express-async-errors'
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 
-
-import sentryConfig from './config/sentry'
+import sentryConfig from './config/sentry';
 
 import './database';
-
-require('dotenv').config();
 
 class App {
   constructor() {
     this.server = express();
 
-    Sentry.init(sentryConfig)
+    Sentry.init(sentryConfig);
 
     this.middlewares();
     this.routes();
-    this.exceptionHandle()
+    this.exceptionHandle();
   }
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -41,14 +40,14 @@ class App {
 
   exceptionHandle() {
     this.server.use(async (err, req, res, next) => {
-      if(process.env.NODE_ENV === 'development') {
-      const errors = await new Youch(err, req).toJSON()
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
 
-      return res.status(500).json(errors)
+        return res.status(500).json(errors);
       }
 
-      return res.status(500).json({error: 'Internal server error.'})
-    })
+      return res.status(500).json({ error: 'Internal server error.' });
+    });
   }
 }
 
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
   host: process.env.MAIL_HOST,
-  port:  process.env.MAIL_PORT,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user:  process.env.MAIL_USER,
-    pass:  process.env.MAIL_PASS,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
