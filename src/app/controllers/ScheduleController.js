759c7dfe187a303a6e60e2c391f8af67d0316c0e
Pedro Henrique <pedrohenrique@Pedros-iMac.local>
2019-11-11 18:59:57 -0300

    schedule erro adjusted

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -6,17 +6,20 @@ import User from '../models/User';
 class ScheduleController {
   async index(req, res) {
     const checkUserProvider = await User.findOne({
-      where: { id: req.userId, provider: true },
+      where: {
+        id: req.userId,
+        provider: true,
+      },
     });
 
     if (!checkUserProvider) {
-      res.status(401).json({ error: 'User is not a provider' });
+      return res.status(401).json({ error: 'User is not a provider' });
     }
 
     const { date } = req.query;
     const parsedDate = parseISO(date);
 
-    const appointment = Appointment.findAll({
+    const appointments = await Appointment.findAll({
       where: {
         provider_id: req.userId,
         canceled_at: null,
@@ -24,17 +27,9 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
-      include: [
-        {
-          model: User,
-          as: 'user',
-          attributes: ['name'],
-        },
-      ],
       order: ['date'],
     });
-
-    return res.json(appointment);
+    return res.json(appointments);
   }
 }
 
